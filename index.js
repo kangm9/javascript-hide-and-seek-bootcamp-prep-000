8d1bc65eccea076d0dfb6b7f2cdb52b8da98eac2
Min Kang <kangm914@gmail.com>
2019-05-01 21:25:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   //pull out all children nodes in ID = grand-node
-  var node=document.querySelectorAll('#grand-node')
+  var node=document.querySelectorAll('#grand-node div')
   
   //find deepest child node
   var deepest = node[node.length - 1];
