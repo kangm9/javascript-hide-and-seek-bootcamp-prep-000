66adca6619a3de778547c060921dce829acec51d
Min Kang <kangm914@gmail.com>
2019-05-01 21:12:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var lis=document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML=(parseInt(lis[i].innerHTML)+n);
+  }
+}
+
+function deepestChild() {
+  
+}
