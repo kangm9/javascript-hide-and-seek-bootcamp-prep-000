81cca3b5eb39052b7d212405987a314f9d9a4632
Min Kang <kangm914@gmail.com>
2019-05-01 21:27:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  //pull out all values of children within class=ranked-list
+  var lis=document.querySelectorAll('.ranked-list')
+  
+  //loop over each value and change it
+  for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML=(parseInt(lis[i].innerHTML)+n);
+  }
+}
+
+function deepestChild() {
+  //pull out all children nodes in ID = grand-node
+  var node=document.querySelectorAll('#grand-node div')
+  
+  //find most deeply nested child element
+  var deepest = node[node.length - 1];
+  return deepest;
+}
